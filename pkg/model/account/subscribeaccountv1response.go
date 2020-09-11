d25a1de509ba68849286227c872126e999218ed8
eynzhang <2427928+eynzhang@users.noreply.github.com>
2020-09-11 09:49:08 +0800

    Fix websocket v1 response field

--- pkg/model/account/subscribeaccountv1response.go
+++ pkg/model/account/subscribeaccountv1response.go
@@ -4,6 +4,8 @@ type SubscribeAccountV1Response struct {
 	Op        string `json:"op"`
 	Timestamp int64  `json:"ts"`
 	Topic     string `json:"topic"`
+	ClientId  string `json:"cid"`
+	ErrorCode int    `json:"err-code"`
 	Data      struct {
 		Event string `json:"event"`
 		List  []struct {
--- pkg/model/order/requestordersv1response.go
+++ pkg/model/order/requestordersv1response.go
@@ -4,7 +4,7 @@ type RequestOrdersV1Response struct {
 	Op        string  `json:"op"`
 	Timestamp int64   `json:"ts"`
 	Topic     string  `json:"topic"`
-	ErrorCode string  `json:"err-code"`
 	ClientId  string  `json:"cid"`
+	ErrorCode int     `json:"err-code"`
 	Data      []Order `json:"data"`
 }
