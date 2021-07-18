5ef52f9cb6e7251f6308bbb9404e9e42fb53d0e8
centny <centny@gmail.com>
2021-07-18 17:36:05 +0800

    modify use default http instance to self

--- internal/httprequest.go
+++ internal/httprequest.go
@@ -1,17 +1,20 @@
 package internal
 
 import (
-	"github.com/huobirdcenter/huobi_golang/logging/perflogger"
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/huobirdcenter/huobi_golang/logging/perflogger"
 )
+
+var DefaultClient = &http.Client{}
 
 func HttpGet(url string) (string, error) {
 	logger := perflogger.GetInstance()
 	logger.Start()
 
-	resp, err := http.Get(url)
+	resp, err := DefaultClient.Get(url)
 	if err != nil {
 		return "", err
 	}
@@ -27,7 +30,7 @@ func HttpPost(url string, body string) (string, error) {
 	logger := perflogger.GetInstance()
 	logger.Start()
 
-	resp, err := http.Post(url, "application/json", strings.NewReader(body))
+	resp, err := DefaultClient.Post(url, "application/json", strings.NewReader(body))
 	if err != nil {
 		return "", err
 	}
