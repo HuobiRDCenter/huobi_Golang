b75a96a256be1177cdf68dfe2d5026a03150f456
“VincentCai” <vincent.cai@htx-inc.com>
2024-04-11 13:05:32 +0800

    sdk接口出入参修改更新及新接口添加

--- pkg/model/order/cancelordersbyidsresponse.go
+++ pkg/model/order/cancelordersbyidsresponse.go
@@ -11,7 +11,7 @@ type CancelOrdersByIdsResponse struct {
 			ClientOrderId string `json:"client-order-id"`
 			ErrorCode     string `json:"err-code"`
 			ErrorMessage  string `json:"err-msg"`
-			OrderState    string `json:"order-state"`
+			OrderState    int    `json:"order-state"`
 		}
 	}
 }
